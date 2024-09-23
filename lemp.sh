1408caf87deb7a1b4072ad9a21a093c39b3478be
root <root@ip-172-31-51-184.ec2.internal>
2024-09-23 08:38:45 +0000

    edit lemp.sh scrit

--- lemp.sh
+++ lemp.sh
@@ -13,7 +13,8 @@ server {
     index index.html index.htm index.php;
 
     location / {
-        try_files $uri $uri/ =404;
+       # try_files $uri $uri/ =404;
+       try_files $uri $uri/ /index.php$is_args$args;
     }
 
     location ~ \.php$ {
